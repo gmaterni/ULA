d7eff5690fad3a4cd367e709ebc658bc7d9b3bf4
giuseppe materni <giuseppe.materni@gmail.com>
2022-05-06 02:59:59 +0200

    06-05-22 (02:59)

--- upd_data_all.py
+++ upd_data_all.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from ulalib.ualog import Log
 from ulalib.update_data import UpdateData
-from ulalib.ula_setting import TEXT_SRC_DIR, CORPUS_NAME
-from pdb import set_trace
+from ulalib.ula_setting import *
 
 __date__ = "09-04-2022"
 __version__ = "0.1.0"
@@ -21,15 +19,11 @@ def update_all_text():
     data_corpus/corpus.form.csv
     """
     try:
-        #set_trace()
-        path_err = "log/upd_all_text.ERR.log"
-        logerr = Log("w").open(path_err, 1).log
         upd_data=UpdateData()
         upd_data.update_all_text_forms()
     except Exception as e:
         msg = f'ERROR upd_all_text \n{e}'
-        logerr(msg)
-        sys.exit()
+        sys.exit(msg)
 
 
 if __name__ == "__main__":
